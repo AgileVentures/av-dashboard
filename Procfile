e8e61905103c77e6cc40fb6fa58d882f69a4497b
mtc2013 <mtc2013opensource@gmail.com>
2018-07-06 23:10:41 -0400

    Changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi.py
+web: gunicorn av_dashboard:app
--- av_dashboard/__init__.py
+++ av_dashboard/__init__.py
@@ -7,5 +7,7 @@ def create_app(test_config=None):
         return "Hello!"
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    create_app().run()
+    app.run()
