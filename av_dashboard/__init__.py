f07d59d48baf6541a295c0ba1e5573c3ff92463e
mtc2013 <mtc2013opensource@gmail.com>
2018-07-07 08:57:58 -0400

    Make some small changes

--- av_dashboard/__init__.py
+++ av_dashboard/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from flask import request, Flask, render_template, make_response
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    from io import StringIO
+
+    def generate_svg():
+        d = {'temperature': [70, 80, 95], 'ice_cream_sales': [10, 25, 40]}
+        df = pd.DataFrame(data=d)
+        df.plot.scatter(x='temperature', y = 'ice_cream_sales', xticks = range(65,100)).plot()
+        figfile = StringIO()
+        plt.savefig(figfile, format='svg')
+        figfile.seek(0)
+        figdata_svg = '<svg' + figfile.getvalue().split('<svg')[1]
+        return figdata_svg
+
     @app.route("/")
     def hello():
-        return "Hello!"
+        return render_template('hello.html', graph = generate_svg())
+
     return app
 
 app = create_app()
